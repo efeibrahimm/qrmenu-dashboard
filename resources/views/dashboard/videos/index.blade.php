f218fb2cfcdb8558dc955396ff35acbff4526fd8
efeibrahimm <ibrahim.efee@hotmail.com>
2023-11-24 18:57:40 +0300

    ssa1

--- resources/views/dashboard/videos/index.blade.php
+++ resources/views/dashboard/videos/index.blade.php
@@ -36,7 +36,7 @@
                                     <div class="card radius-10">
                                         <div class="card-body">
                                             <div class="d-flex align-items-start gap-2">
-                                                <video src="{{ asset('images/' . $item->url) }}"></video>
+                                                <video src="{{ asset('images/' . $item->url) }}" style="width: 100%"></video>
                                                
                                             </div>
                                             <div class="d-flex align-items-center mt-3">
